857ff4d2e2adfb9f03db58621dff23d4bec1082c
android <rakhatalientertainment@gmail.com>
2023-02-20 14:58:06 +0600

    First

---
+++ app/src/main/java/com/example/qonzhyq/ui/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.qonzhyq.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.qonzhyq.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/qonzhyq/ui/fragments/MessageFragment.kt
@@ -0,0 +1,20 @@
+package com.example.qonzhyq.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.qonzhyq.R
+
+class MessageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_message, container, false)
+    }
+
+}
